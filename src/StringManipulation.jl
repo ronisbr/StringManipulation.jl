9ed3de49bdb759c73708ae1a810e2a5ea2596d38
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-03-01 15:39:23 -0300

    :bug: Push forgotten modifications

--- src/StringManipulation.jl
+++ src/StringManipulation.jl
@@ -9,10 +9,19 @@ import Base: convert, @kwdef
 const _CSI = "\x1b["
 # Regex that removes all ANSI escape sequences.
 const _REGEX_ANSI = r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])"
+# Escape sequence that reset all decorations.
+const _RESET_DECORATIONS = _CSI * "0m"
 
 ################################################################################
 #                                  Structures
 ################################################################################
+
+# Enumetation to store the state in `Decoration`.
+@enum DecorationState begin
+    inactive  = 0
+    active    = 1
+    unchanged = 2
+end
 
 export Decoration
 
@@ -22,12 +31,12 @@ export Decoration
 Structure to hold the current decoration of a string.
 """
 @kwdef struct Decoration
-    foreground::String = ""
-    background::String = ""
-    bold::Bool         = false
-    underline::Bool    = false
-    reset::Bool        = false
-    reversed::Bool     = false
+    foreground::String         = ""
+    background::String         = ""
+    bold::DecorationState      = unchanged
+    underline::DecorationState = unchanged
+    reset::Bool                = false
+    reversed::DecorationState  = unchanged
 end
 
 const _DEFAULT_DECORATION = Decoration()
