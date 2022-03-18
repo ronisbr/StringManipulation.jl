768a07f3c80526c804aa1d8c5d789cfd9002304e
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-03-18 20:35:44 -0300

    :sparkles: Add func. to return the crop to fit str in field

--- src/crop.jl
+++ src/crop.jl
@@ -7,7 +7,8 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-export left_crop, fit_string_in_field, right_crop
+export left_crop, fit_string_in_field, get_crop_to_fit_string_in_field
+export right_crop
 
 """
     left_crop(str::AbstractString, crop_width::Int)
@@ -83,56 +84,24 @@ function fit_string_in_field(
     keep_ansi::Bool = true,
     printable_string_width::Int = -1
 )
-
     str_width = printable_string_width < 0 ?
         printable_textwidth(str) :
         printable_string_width
 
-    Δ = str_width - field_width
+    crop = get_crop_to_fit_string_in_field(
+        str,
+        field_width;
+        add_continuation_char,
+        add_space_in_continuation_char,
+        continuation_char,
+        printable_string_width = str_width
+    )
 
-    # If the field is larger than the string, then just return it.
-    if Δ ≤ 0
+    if crop == 0
         return str
     end
 
-    # If the user is asking for the continuation char, then we must crop the
-    # string to account for the continuation char.
-    cont_str = ""
-
-    if add_continuation_char
-        if crop_side == :right
-
-            if add_space_in_continuation_char
-                cont_str = " " * string(continuation_char)
-            else
-                cont_str = string(continuation_char)
-            end
-
-        else
-            cont_str = string(continuation_char)
-
-            if add_space_in_continuation_char
-                cont_str *= " "
-            end
-        end
-
-        wcont_str = textwidth(cont_str)
-
-        Δ += wcont_str
-
-        # If we are left with no space, then just return the continuation char.
-        if Δ > str_width
-            # We just need to check if the user wants to keep the ANSI escape
-            # sequences.
-            ansi = ""
-
-            if keep_ansi
-                ansi = get_decorations(str)
-            end
-
-            return string(continuation_char) * ansi
-        end
-    end
+    cont_str = add_continuation_char ? string(continuation_char) : ""
 
     # Crop from the right
     # ==========================================================================
@@ -140,25 +109,95 @@ function fit_string_in_field(
     if crop_side == :right
         cropped_str, ansi = right_crop(
             str,
-            Δ;
+            crop;
             keep_escape_seq = keep_ansi,
             printable_string_width = str_width
         )
 
-        return cropped_str * cont_str * ansi
+        if add_space_in_continuation_char
+            return cropped_str * " " * cont_str * ansi
+        else
+            return cropped_str * cont_str * ansi
+        end
 
     # Crop from the left
     # ==========================================================================
 
     else
-        ansi, cropped_str = left_crop(str, Δ)
+        ansi, cropped_str = left_crop(str, crop)
 
-        if keep_ansi
-            return ansi * cont_str * cropped_str
+        result = keep_ansi ? ansi : ""
+
+        if add_space_in_continuation_char
+            result *= cont_str * " " * cropped_str
         else
-            return cont_str * cropped_str
+            result *= cont_str * cropped_str
+        end
+
+        return result
+    end
+end
+
+"""
+    get_crop_to_fit_string_in_field(str::AbstractString, field_width::Int; kwargs...)
+
+Get the number of printable characters to be cropped so that the string `str`
+can fit in the field with size `field_width`.
+
+# Keywords
+
+- `add_continuation_char::Bool`: If `true`, a continuation character is added to
+    the cropped string end. Notice that the returned string always has a
+    printable width of `field_size`. (**Default** = `true`)
+- `add_space_in_continuation_char::Bool`: If `true`, a space is added before the
+    continuation character if `crop_size` is `:right`, or after the continuation
+    character if `crop_side` if `:left`. (**Default** = `false`)
+- `continuation_char::Char`: The continuation character to add if the string is
+    cropped. (**Default** = `…`)
+- `printable_string_width::Int`: Provide the printable string width to reduce
+    the computational burden. If this parameters is lower than 0, the printable
+    width is compute internally. (**Default** = -1)
+"""
+function get_crop_to_fit_string_in_field(
+    str::AbstractString,
+    field_width::Int;
+    add_continuation_char::Bool = true,
+    add_space_in_continuation_char::Bool = false,
+    continuation_char::Char = '…',
+    printable_string_width::Int = -1
+)
+
+    str_width = printable_string_width < 0 ?
+        printable_textwidth(str) :
+        printable_string_width
+
+    Δ = str_width - field_width
+
+    # If the field is larger than the string, then we do not need to crop.
+    if Δ ≤ 0
+        return 0
+    end
+
+    # If the user is asking for the continuation char, then we must crop the
+    # string to account for the continuation char.
+    cont_str = ""
+
+    if add_continuation_char
+        cont_str_width = textwidth(continuation_char)
+
+        if add_space_in_continuation_char
+            cont_str_width += 1
+        end
+
+        Δ += cont_str_width
+
+        # If we are left with no space, then we must crop the entire string.
+        if Δ > str_width
+            return str_width
         end
     end
+
+    return Δ
 end
 
 """
